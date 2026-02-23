3b3527942bb066d3028f7809107ddb5d1a8d3027
alvaro.hermida <alvaro.hermida@coinscrap.com>
2026-02-23 07:59:33 +0100

    primer commit

---
+++ db/mysql.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS `daaexample`;
+CREATE DATABASE `daaexample`;
+
+CREATE TABLE `daaexample`.`people` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`surname` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `daaexample`.`users` (
+	`login` varchar(100) NOT NULL,
+	`password` varchar(64) NOT NULL,
+	`role` varchar(10) NOT NULL,
+	PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE USER IF NOT EXISTS 'daa'@'localhost' IDENTIFIED WITH mysql_native_password BY 'daa';
+GRANT ALL ON `daaexample`.* TO 'daa'@'localhost';
